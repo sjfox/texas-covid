dea000d7ad6c1cc467ecfc2a3f193031853b867e
Spencer Fox <spencerfox@Spencers-MacBook-Pro.local>
2020-06-24 16:47:06 -0600

    update with maps

--- code/create-figures.R
+++ code/create-figures.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readxl)
 library(lubridate)
 library(cowplot)
+library(sf)
 theme_set(theme_cowplot())
 
 
@@ -31,13 +32,14 @@ tsa_df %>%
           legend.position = "none") +
   labs(x = "", y = "Current hospitalized")
   
-tsa_df %>% 
+tsa_growth <- tsa_df %>% 
   mutate(hospitalizations = hospitalizations + 1) %>% 
   filter(date == ymd("2020-06-01") | date == max(date)) %>% 
   spread(date, hospitalizations) %>% 
   mutate(percent_change = (`2020-06-22`) / `2020-06-01`,
          trend = ifelse(percent_change > 1, "Increasing", "Decreasing")) %>% 
-  mutate(tsa_lab = forcats::fct_reorder(tsa, percent_change)) %>% 
+  mutate(tsa_lab = forcats::fct_reorder(tsa, percent_change)) 
+tsa_growth %>%   
   ggplot(aes(tsa_lab, percent_change, fill = trend)) + 
     geom_col() +
     scale_y_continuous(labels = scales::percent) +
@@ -50,3 +52,17 @@ tsa_df %>%
   
 ## Trauma areas
 ## https://www.dshs.texas.gov/emstraumasystems/etrarac.shtm
+
+## Make maps
+
+tx_mappings <- read_csv("raw-data/tsa_county_mapping.csv", col_types = "cccc")
+
+usmap::us_map("counties") %>% 
+  inner_join(tx_mappings, c("fips")) %>% 
+  left_join(tsa_growth, by = c("tsa_name" = "tsa")) %>% 
+  ggplot(aes(x,y, group = fips, fill = percent_change)) + 
+    geom_polygon() +
+    theme_map() +
+    scale_fill_viridis_c(labels = scales::percent) 
+
+
